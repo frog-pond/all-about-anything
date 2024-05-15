ff544f85a5d980169387845d2e960821cec9f2c3
Hawken Rives <hawkrives@fastmail.fm>
2024-05-15 16:50:39 -0400

    Create the iOS app project

---
+++ AllAboutAnything/AllAboutAnythingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllAboutAnythingApp.swift
+//  AllAboutAnything
+//
+//  Created by Hawken Rives on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct AllAboutAnythingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
